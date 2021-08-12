ef03ccbbd536c1f347c3b1ba2bb771014fc937a8
Xiao xi <taogu163@163.com>
2021-08-12 22:24:57 +0800

    2

--- usr/static/demo/assets/js/main.js
+++ usr/static/demo/assets/js/main.js
@@ -25,7 +25,7 @@ function recommend(type = "illust") {
     type: "GET",
     data: {
       type: type,
-      totalPage: totalPage,
+      totalPage: 5,
       groupIndex: 0,
       sortMode: 0,
       isSort: 0,
@@ -381,31 +381,21 @@ function showPics(
 
       // 最终结果
       rstHtml +=
-        '<div class="card-group" id = "' +
-        data[i]["id"] +
-        '"><div class="card position-relative">' +
-        '<div id="role" class="img position-absolute top-50 start-50 translate-middle"><div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div></div>' +
-        '<a href="' +
-        imgUrl +
-        '">' +
-        '<img src="' +
+        '<div class="col-sm-6 col-md-5 col-lg-4 mb-4"><div class="card">' +
+        '<svg  class="bd-placeholder-img card-img-top" width="100%"  xmlns="' +
         imgUrlCover +
-        ' " class="card-img-top img-items"' +
-        'alt="' +
-        data[i]["title"] +
-        '"/></a>' +
-        '<div class="card-body">' +
-        '<h5 class="card-title">' +
-        data[i]["title"] +
-        '</h5><h6 class="card-subtitle mb-2 text-muted">' +
-        extra +
-        '</h6><p class="card-text">' +
-        extraText +
-        "</p></div></div></div>";
+        '" role="img" ' +
+        ' aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false" >' +
+        '<title>Placeholder</title> <rect width="100%" height="100%" fill="#868e96" /><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>' +
+        '<div class="card-body"><h5 class="card-title">Card title that wraps to a new line</h5> <p class="card-text">' +
+        "This is a longer card with supporting text below as a natural" +
+        "lead-in to additional content. This content is a little bit" +
+        " longer." +
+        "</p> </div></div>";
     }
 
     // 输出
-    $("#Card-img").html(rstHtml);
+    $("#img-items").html(rstHtml);
     $(".img").hide();
     $(".progress-bar").animate("width", "100%");
   }
